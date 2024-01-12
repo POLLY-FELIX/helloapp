27dffe3065eb32812ad6ad190a1772a34abba5b6
perola polyana <perola.polly.felix@gmail.com>
2024-01-11 21:57:02 -0300

    primeiro comite

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Imprime um texto na tela.
+using System.Runtime.InteropServices.Marshalling;
+
+Console.WriteLine("Hello World!");
+
+Console.WriteLine("Seja bem-vindo ao meu primeiro programa");
+
+// Criação de duas variáveis.
+string nome;
+int idade;
+
+Console.WriteLine("Qual seu nome?");
+
+// Lê o nome digitado pelo usuario e armazena na variável nome.
+nome = Console.ReadLine();
+
+Console.WriteLine("Prazer em conhecer")
